07e286e6fcd2a5d8b81687567542e67ed8c2d1ba
Sneha Muttanna Chinchakandi <01fe23bcs377@kletech.ac.in>
2025-11-26 16:35:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log("MONGO_URI:", process.env.MONGO_URI);
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI is not defined in environment variables");
+    }
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected Successfully");
   } catch (error) {
